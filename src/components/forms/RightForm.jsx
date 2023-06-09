9ffa3bd074a60491487f3279f262b678bf9d8f6e
Aryan <aryan34.model@gmail.com>
2023-06-09 10:02:52 +0530

    loader added

--- src/components/forms/RightForm.jsx
+++ src/components/forms/RightForm.jsx
@@ -7,6 +7,7 @@ export default function RightForm() {
 
 
 
+  const [showlodaer, setshowlodaer] = useState(false)
   const [showForm, setshowForm] = useState(true)
   const [user_added, setuser_added] = useState(false)
   const [glob_otp, setglob_otp] = useState('')
@@ -35,84 +36,86 @@ export default function RightForm() {
       {/*  main form details */}
 
       {showForm === true && user_added === false ?
-
-        <form >
-          <h6 id='error_msg'>Enter all fields</h6>
-          <div id="name">
-            <h3>Name </h3>
-            <input type="text" placeholder='First Name' onChange={(e) => { setFname(e.target.value) }} id='F_email' />
-            <input type="text" placeholder='Last Name' onChange={(e) => { setLname(e.target.value) }} id='L_email' />
-          </div>
-          <div id="email">
-            <h3>E-mail  </h3>
-            <input type="text" placeholder='xyz1@gmail.com' onChange={(e) => { setemail(e.target.value) }} id='I_email' />
-          </div>
-          <div id="contact">
-            <h3>Contact  </h3>
-            <input type="text" placeholder='98xxxxxxx' onChange={(e) => { setcontact(e.target.value) }} id='I_contact' />
-          </div>
-          <div id="dropdown">
-
-            <div id="Branch">
-              <h3>Branch  </h3>
-              <select name="branch" id='branch' onChange={(e) => { setbranch(e.target.value) }} >
-                <optgroup label="MUMBAI">
-                  <option value="Dadar">Dadar</option>
-                  <option value="Dombivli" defaultValue>Dombivli</option>
-                </optgroup>
-                <optgroup label="PUNE">
-                  <option value="mercedes">Mercedes</option>
-                  <option value="audi">Audi</option>
-                </optgroup>
-              </select>
+        //  show loader 
+        (showlodaer === true ? <div className="loader_div"><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div> </div> :
+          <form >
+            <h6 id='error_msg'>Enter all fields</h6>
+            <div id="name">
+              <h3>Name </h3>
+              <input type="text" placeholder='First Name' onChange={(e) => { setFname(e.target.value) }} id='F_email' />
+              <input type="text" placeholder='Last Name' onChange={(e) => { setLname(e.target.value) }} id='L_email' />
             </div>
-            <div id="GateYear" onChange={(e) => { setGateYear(e.target.value) }}>
-              <h3>GATE year  </h3>
-              <select name="gateYear" id='gateYear'>
-                <option value="2024" >2024</option>
-                <option value="2025" defaultValue>2025</option>
-                <option value="2026">2026</option>
-                <option value="2027">2027</option>
-                <option value="2028">2028</option>
-              </select>
+            <div id="email">
+              <h3>E-mail  </h3>
+              <input type="text" placeholder='xyz1@gmail.com' onChange={(e) => { setemail(e.target.value) }} id='I_email' />
             </div>
-          </div>
-          <div id="btn">
-            <button onClick={submit_Btn} >Continue </button>
+            <div id="contact">
+              <h3>Contact  </h3>
+              <input type="text" placeholder='98xxxxxxx' onChange={(e) => { setcontact(e.target.value) }} id='I_contact' />
+            </div>
+            <div id="dropdown">
 
-          </div>
-          <h6>
-            Get free GATE study plan (Note: 1 year study plan for 2025)
-          </h6>
+              <div id="Branch">
+                <h3>Branch  </h3>
+                <select name="branch" id='branch' onChange={(e) => { setbranch(e.target.value) }} >
+                  <optgroup label="MUMBAI">
+                    <option value="Dadar">Dadar</option>
+                    <option value="Dombivli" defaultValue>Dombivli</option>
+                  </optgroup>
+                  <optgroup label="PUNE">
+                    <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+                  </optgroup>
+                </select>
+              </div>
+              <div id="GateYear" onChange={(e) => { setGateYear(e.target.value) }}>
+                <h3>GATE year  </h3>
+                <select name="gateYear" id='gateYear'>
+                  <option value="2024" >2024</option>
+                  <option value="2025" defaultValue>2025</option>
+                  <option value="2026">2026</option>
+                  <option value="2027">2027</option>
+                  <option value="2028">2028</option>
+                </select>
+              </div>
+            </div>
+            <div id="btn">
+              <button onClick={submit_Btn} >Continue </button>
 
-        </form>
+            </div>
+            <h6>
+              Get free GATE study plan (Note: 1 year study plan for 2025)
+            </h6>
+          </form>
 
-        :
+        ) :
 
         // {/* otp form  */}
         (
           user_added === false ?
-            <div id='OTP_form'>
 
-              <div id="OTP" >
-                <h3>OTP has been sent to your email   </h3>
-                <h3 style={{textDecorationLine:'underline',color:'green',fontWeight:'400'}}> {email} </h3>
-                {/* <input type="text" placeholder='xxxxxx' onChange={(e) => { setnew_otp(e.target.value) }} /> */}
-                <div className="otp_box">
-                  <input type="text" id='ist' maxLength="1" onKeyUp={(e) => { clickEvent(e.target.value, 'sec') }} />
-                  <input type="text" id="sec" maxLength="1" onKeyUp={(e) => { clickEvent(e.target.value, 'third') }} />
-                  <input type="text" id="third" maxLength="1" onKeyUp={(e) => { clickEvent(e.target.value, 'fourth') }} />
-                  <input type="text" id="fourth" maxLength="1" onKeyUp={(e) => { clickEvent(e.target.value, 'fifth') }} />
-                  <input type="text" id="fifth" maxLength="1" />
+            // show loader
+            (showlodaer === true ? <div className="loader_div"><div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div> </div> :
+              <div id='OTP_form'>
+                <div id="OTP" >
+                  <h3>OTP has been sent to your email   </h3>
+                  <h3 style={{ textDecorationLine: 'underline', color: 'green', fontWeight: '400' }}> {email} </h3>
+                  {/* <input type="text" placeholder='xxxxxx' onChange={(e) => { setnew_otp(e.target.value) }} /> */}
+                  <div className="otp_box">
+                    <input type="text" id='ist' maxLength="1" onKeyUp={(e) => { clickEvent(e.target.value, 'sec') }} />
+                    <input type="text" id="sec" maxLength="1" onKeyUp={(e) => { clickEvent(e.target.value, 'third') }} />
+                    <input type="text" id="third" maxLength="1" onKeyUp={(e) => { clickEvent(e.target.value, 'fourth') }} />
+                    <input type="text" id="fourth" maxLength="1" onKeyUp={(e) => { clickEvent(e.target.value, 'fifth') }} />
+                    <input type="text" id="fifth" maxLength="1" />
+                  </div>
+
                 </div>
-
+                <div id="btn">
+                  <h5 id='error_otp_msg' style={{ color: 'red' }}></h5>
+                  <button onClick={addUser}>Confirm </button>
+                </div>
               </div>
-              <div id="btn">
-                <h5 id='error_otp_msg' style={{ color: 'red' }}></h5>
-                <button onClick={addUser}>Confirm </button>
-              </div>
-            </div>
-            : <div id='success'>
+            ): <div id='success'>
               <img src={sucs} alt="" />
               <h3>Application submitted</h3>
               <h4 style={{ color: 'crimson' }}>we will contact you ASAP!!!</h4>
@@ -187,13 +190,14 @@ export default function RightForm() {
     setglob_otp(Gen_OTP)
     console.log(Gen_OTP);
 
-
+    setshowlodaer(true)
 
     const data = { email, Gen_OTP }
     // const result = axios.post('http://localhost:8000/api/sndOTP', data, { withCredentials: true }).then((res) => {
-    const result = axios.post('https://landingpage77-backend.onrender.com/api/sndOTP', data, { withCredentials: true }).then((res) => {
+      const result = axios.post('https://landingpage77-backend.onrender.com/api/sndOTP', data, { withCredentials: true }).then((res) => {
       console.log(res.data);
       res.data.success === true ? setshowForm(false) : setshowForm(true);
+      setshowlodaer(false)
     })
 
 
@@ -229,13 +233,17 @@ export default function RightForm() {
 
 
     if (glob_otp === newTop) {
-      const data = { first_name:Fname, last_name:Lname, email, contact, branch, GateYear }
 
 
-      const result = axios.post('https://landingpage77-backend.onrender.com/api/newUser', data, { withCredentials: true })
+      setshowlodaer(true)
+      const data = { first_name: Fname, last_name: Lname, email, contact, branch, GateYear }
+      // const result = axios.post('http://localhost:8000/api/newUser', data, { withCredentials: true })
+        const result = axios.post('https://landingpage77-backend.onrender.com/api/newUser', data, { withCredentials: true })
         .then((res) => {
           console.log(res.data)
           setuser_added(res.data.success)
+          setshowlodaer(false);
+
         })
 
     } else {
